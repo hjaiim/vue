571f6a833081d4fd71d7ea528ba200f4c09c989f
12WEI_smile <1846638289@qq.com>
2017-12-08 17:36:45 +0800

    add:注册、登录、重置密码页面

--- src/pages/register/register.vue
+++ src/pages/register/register.vue
@@ -1,5 +1,37 @@
 <template>
-	<div>
+	<div class="wrap-page register-wrap relative">
+		<div class="inner-register is-transformed">
+			<p class="register-tit">创客沃土注册</p>
+			<div class="form-wrap">
+				<div class="register-form">
+					<p>登录名:</p>
+					<input class="user-name border-gray-light ani-time" type="text" placeholder="请输入登录名：">
+				</div>
+			</div>
+			<div class="form-wrap">
+				<div class="register-form">
+					<p>姓名:</p>
+					<input class="phone-num border-gray-light ani-time" type="text" placeholder="请输入您的姓名：">
+				</div>
+			</div>
+			<div class="form-wrap">
+				<div class="register-form">
+					<p>密码:</p>
+					<input class="phone-num border-gray-light ani-time" type="password" placeholder="请输入密码：">
+				</div>
+			</div>
+			<div class="form-wrap">
+				<div class="register-form">
+					<p>重复密码:</p>
+					<input class="phone-num border-gray-light ani-time" type="password" placeholder="请再输入密码：">
+				</div>
+			</div>
+			<div class="action-wrap">
+				<span class="back-btn ani-time pointer left">返回</span>
+				<span class="register-btn ani-time pointer right">注册</span>
+			</div>
+
+		</div>
 	</div>
 </template>
 <script type="text/ecmascript-6">
@@ -16,6 +48,7 @@
 		methods: {}
 	}
 </script>
-<style type="text/css" lang="sass" rel="stylesheet/css">
-
+<style type="text/css" lang="sass" rel="stylesheet/css" scoped>
+	@import "../../css/mixin.scss";
+	@import "../../css/register.scss";
 </style>
--- src/pages/resetPwd/resetPwd.vue
+++ src/pages/resetPwd/resetPwd.vue
@@ -1,5 +1,43 @@
 <template>
-	<div>
+	<div class="wrap-page register-wrap relative">
+		<div class="inner-register is-transformed">
+			<p class="register-tit">创客沃土</p>
+			<div class="form-wrap">
+				<div class="register-form">
+					<p>登录名:</p>
+					<input class="user-name border-gray-light ani-time" type="text" placeholder="请输入登录名：">
+				</div>
+			</div>
+			<div class="form-wrap">
+				<div class="register-form">
+					<p>手机号码:</p>
+					<input class="phone-num border-gray-light ani-time" type="text" placeholder="请输入手机号：">
+				</div>
+			</div>
+			<div class="form-wrap">
+				<div class="register-form">
+					<p>验证码:</p>
+					<div class="input-wrap">
+						<input class="verify-code border-gray-light ani-time" type="text" placeholder="请输入验证码：">
+						<span class="send-btn pointer right ani-time">验证码
+						</span>
+					</div>
+				</div>
+			</div>
+			<div class="form-wrap">
+				<div class="register-form">
+					<p>新密码:</p>
+					<input class="phone-num border-gray-light ani-time" type="password" placeholder="请输入密码：">
+				</div>
+			</div>
+			<div class="form-wrap">
+				<div class="register-form">
+					<p>重复密码:</p>
+					<input class="phone-num border-gray-light ani-time" type="password" placeholder="请再输入密码：">
+				</div>
+			</div>
+			<div class="sure-btn pointer ani-time">确定</div>
+		</div>
 	</div>
 </template>
 <script type="text/ecmascript-6">
@@ -16,6 +54,7 @@
 		methods: {}
 	}
 </script>
-<style type="text/css" lang="sass" rel="stylesheet/css">
-
+<style type="text/css" lang="sass" rel="stylesheet/css" scoped>
+	@import "../../css/mixin.scss";
+	@import "../../css/register.scss";
 </style>
