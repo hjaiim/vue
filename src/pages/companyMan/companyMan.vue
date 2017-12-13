3190ec8fe50b5abeacbbe9c67b80c5df356393e2
12WEI_smile <1846638289@qq.com>
2017-12-13 19:06:45 +0800

    update

--- src/pages/companyMan/companyMan.vue
+++ src/pages/companyMan/companyMan.vue
@@ -1,5 +1,22 @@
 <template>
-	<com-layout></com-layout>
+	<com-layout>
+		<div class="plat-wrap">
+			<div class="action-wrap clear">
+				<span class="add-btn action-btn ani-time left pointer">
+					<i class="cross-txt relative"></i>
+					<span>新增</span>
+				</span>
+				<div class="search-wrap clear left">
+					<span class="left">公司名称</span>
+					<div class="search-bar left relative">
+						<input type="text" class="search-name">
+						<img class="close-btn absolute pointer" :src="g.path.images+'/close-popup.png'" alt="">
+					</div>
+				</div>
+				<span class="search-btn action-btn ani-time left pointer">查询</span>
+			</div>
+		</div>
+	</com-layout>
 </template>
 <script type="text/ecmascript-6">
 	import g from "../../global";
@@ -13,12 +30,13 @@
 				g: g,
 			}
 		},
-		components:{
+		components: {
 			ComLayout
 		},
 		methods: {}
 	}
 </script>
-<style type="text/css" lang="sass" rel="stylesheet/css">
-
+<style type="text/css" lang="sass" rel="stylesheet/css" scoped>
+	@import "../../css/mixin.scss";
+	@import "../../css/platManage.scss";
 </style>
