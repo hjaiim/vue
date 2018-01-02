4a55c78aab85aec2f6d58a05d3430140c9155768
12WEI_smile <1846638289@qq.com>
2018-01-02 14:27:56 +0800

    update

--- libs/config.dev.js
+++ libs/config.dev.js
@@ -69,8 +69,8 @@ var WEB_CONFIG = {
 		emailReg: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
 		codeReg: /\d{4}/,
 		ossUrl: "http://test.boms.huanwuh5.com/boms/upload/",
-		uploadAccess: "http://121.41.80.45:8001/file/upload",
-		delPicAccess: "http://121.41.80.45:8001/file/del",
+		uploadAccess: "http://192.168.12.219:8001/file/upload",
+		delPicAccess: "http://192.168.12.219:8001/file/del",
 		timeoutClock:10,
 		pageSize:10
 	}
