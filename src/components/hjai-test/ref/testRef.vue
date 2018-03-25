f5ed71ad2342a6df2506d20004b307220513afc6
haojun <hjaiim@163.com>
2018-03-25 14:43:23 +0800

    add:vue--ref属性

---
+++ src/components/hjai-test/ref/testRef.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<p>{{name}}</p>
+
+		<div @click="onClick_traceRef">打印ref</div>
+		<div ref="sex" id="sexDiv">man</div>
+		<ul v-for="item in arr">
+			<li ref="item">{{item}}</li>
+		</ul>
+	</div>
+	<!--ref给元素祖册引用信息
+	1.如果注册的是普通dom元素,引用指向的就是这个当前元素,等同于获取dom元素.
+	2.如果注册的是子组件,引用的就是这个组件的实例,那么实例上面的属性自然可以用.
+	3.如果和v-for一起用,那么引用的ref是个数组.
+		-->
+
+	<!--$refs
+	一个对象类型,持有所有ref注册的dom元素
+	-->
+</template>
+<script type="text/ecmascript-6">
+	import g from "../../../global";
+	export default{
+		created(){
+
+		},
+		data(){
+			return {
+				g: g,
+				name: 'hjai',
+				arr: [1, 2, 4]
+			}
+		},
+		components: {},
+		watch: {},
+		methods: {
+			onClick_repaint($v){
+				this.name = $v + 'hjai';
+				console.log('重新渲染子组件dom');
+			},
+			onClick_traceRef()
+			{
+				console.log(this.$refs.item[1]);
+				console.log(this.$refs.sex);
+				console.log(document.getElementById('sexDiv'));
+			}
+		}
+	}
+
+</script>
+<style type="text/css" lang="sass" rel="stylesheet/css" scoped>
+
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,80 +1,70 @@
 <template>
-    <div>
-        <!--vue,is-->
-        <di is="hjTestIs" class="test-is">
-            我是内容
-        </di>
+	<div>
+		<!--vue,is-->
+		<di is="hjTestIs" class="test-is">
+			我是内容
+		</di>
 
-        <!--slot-->
-        <test-slot1 class="test-slot1">
-            <span>我是父组件内容1</span>
-            <span>我是父组件内容2</span>
-        </test-slot1>
+		<!--slot-->
+		<test-slot1 class="test-slot1">
+			<span>我是父组件内容1</span>
+			<span>我是父组件内容2</span>
+		</test-slot1>
 
-        <!--具名slot-->
-        <test-slot2 class="test-slot2">
-            <span>我是父组件内容1</span>
-            <div name="header">
-                <span>我是父组件header内容1</span>
-            </div>
-            <div name="footer">
-                <span>我是父组件footer内容1</span>
-            </div>
-        </test-slot2>
+		<!--具名slot-->
+		<test-slot2 class="test-slot2">
+			<span>我是父组件内容1</span>
+			<div name="header">
+				<span>我是父组件header内容1</span>
+			</div>
+			<div name="footer">
+				<span>我是父组件footer内容1</span>
+			</div>
+		</test-slot2>
 
-        <!--组件传值-->
-        <pass-value :age="27" @change="onCLik_getValue"><span>父组件的值为27</span></pass-value>
-    </div>
+		<!--组件传值-->
+		<pass-value :age="27" @change="onCLik_getValue"><span>父组件的值为27</span></pass-value>
+
+		<!--ref-->
+		<button @click="onClick_changeName">ref按钮</button>
+		<test-ref ref="refExample"></test-ref>
+	</div>
 </template>
 <script type="text/ecmascript-6">
-    import g from "../../global";
-    import hjTestIs from "../../components/hjai-test/is/hjTestIs.vue"
-    import testSlot1 from "../../components/hjai-test/slot/testSlot1.vue"
-    import testSlot2 from "../../components/hjai-test/slot/testSlot2.vue"
-    import passValue from "../../components/hjai-test/passValue/passValue.vue"
-    export default{
-        created(){
+	import g from "../../global";
+	import hjTestIs from "../../components/hjai-test/is/hjTestIs.vue"
+	import testSlot1 from "../../components/hjai-test/slot/testSlot1.vue"
+	import testSlot2 from "../../components/hjai-test/slot/testSlot2.vue"
+	import passValue from "../../components/hjai-test/passValue/passValue.vue"
+	import testRef from "../../components/hjai-test/ref/testRef.vue"
+	export default{
+		created(){
 
-        },
-        data(){
-            return {
-                g: g
-            }
-        },
-        components: {
-            hjTestIs,
-            testSlot1,
-            testSlot2,
-            passValue
-        },
-        watch: {
-        },
-        methods:{
-            onCLik_getValue($v){
-                console.log($v);
-            }
-        }
-    }
+		},
+		data(){
+			return {
+				g: g
+			}
+		},
+		components: {
+			hjTestIs,
+			testSlot1,
+			testSlot2,
+			passValue,
+			testRef
+		},
+		watch: {},
+		methods: {
+			onCLik_getValue($v){
+				console.log($v);
+			},
+			onClick_changeName()
+			{
+				this.$refs.refExample.onClick_repaint('帅气的');
+			}
+		}
+	}
 
 </script>
 <style type="text/css" lang="sass" rel="stylesheet/css" scoped>
-    @import "../../css/mixin.scss";
-    @import "../../css/percenter.scss";
-
-    .file-input {
-        width: 120px;
-        height: 120px;
-        position:absolute;
-        top: 0;
-        left: 0;
-        z-index: 2;
-        filter: alpha(opacity=0);
-        opacity: 0;
-
-    }
-
-    .err-msg {
-        color: #ed5564;
-
-    }
 </style>
